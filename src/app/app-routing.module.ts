1795127be1130df29671411c0674e5735628852d
Strada Fabio <fabio.strada@allitude.it>
2022-02-21 15:48:58 +0100

    update dependences

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'sub-module1',
+    loadChildren: () => import('@test/sub-module1').then((m) => m.SubModule1Module)
+  },
+  {
+    path: 'sub-module2',
+    loadChildren: () => import('@test/sub-module2').then((m) => m.SubModule2Module)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
